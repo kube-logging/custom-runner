8b5af9bcaf08da3869b7cd2e11e6179d27106417
Bence Csati <bence.csati@axoflow.com>
2026-04-09 13:38:27 +0200

    chore: update license-headers to match kube-logging's
    Signed-off-by: Bence Csati <bence.csati@axoflow.com>

--- src/config/config.go
+++ src/config/config.go
@@ -1,8 +1,21 @@
-// Copyright (c) 2022 Cisco All Rights Reserved.
+// Copyright © 2022 Cisco Systems, Inc. and/or its affiliates
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package config
 
 import (
-	"fmt"
+	"errors"
 	"io/ioutil"
 
 	"github.com/kube-logging/custom-runner/src/events"
@@ -48,7 +61,7 @@ func (c *Config) ActionsForEvent(args []interface{}) ([]Action, error) {
 	args = append([]interface{}{"events"}, args...)
 	acts := c.GetIn(args...)
 	if acts == nil {
-		return nil, fmt.Errorf(ErrNotFound)
+		return nil, errors.New(ErrNotFound)
 	}
 	var actions []Action
 
